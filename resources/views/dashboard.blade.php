ae9f1aacd537d7ba18dfa22b241144db110009d3
Niklas Canisius <niklascanisius@me.com>
2021-06-29 16:00:52 +0200

    optimize order page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,9 @@
                         Werden versendet an
                         <input type="email" name="email" value="{{ auth()->user()->email }}" title="email" readonly>
                         <br>
-                        <button type="submit">Bestellen</button>
+                        <button type="submit" class="p-3 bg-gray-100 shadow rounded">
+                            Bestellen
+                        </button>
                     </form>
                 </div>
             </div>
@@ -40,6 +42,11 @@
     <div class="pt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h3 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Bestellhistorie') }}
+                    </h3>
+                </div>
                 @foreach(auth()->user()->orders()->orderBy('id', 'desc')->get() as $order)
                     <div class="p-6 bg-white border-b border-gray-200">
                         {{ $order->created_at }}:
