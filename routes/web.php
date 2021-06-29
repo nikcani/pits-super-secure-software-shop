ff12e245dee8d27f496df87fe7798528290220d3
Niklas Canisius <niklascanisius@me.com>
2021-06-29 12:42:11 +0200

    add basic functionality

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/',
+    function () {
+        return view('welcome');
+    }
+);
+
+Route::get(
+    '/dashboard',
+    function () {
+        return view('dashboard');
+    }
+)->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
+
+Route::get(
+    '/order',
+    function (Request $request) {
+        /*if (csrf_token() !== $request->get('_token')) {
+            abort(403, 'csrf protection');
+        }*/
+        return "Sie haben erfolgreich " . $request->get(
+                'amount'
+            ) . " Lizenzen bestellt. Die Lizenzschlüssel wurden an die Adresse " . $request->get(
+                'email'
+            ) . " verschickt.";
+        return [
+            'session_token' => csrf_token(),
+            'user' => auth()->user(),
+            'request' => $request->all(),
+        ];
+    }
+)->middleware(['auth']);
